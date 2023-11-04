6b816bfc8b1adc21637f5a689e50dd4ec77556ba
alp.yurtseven <alpyurtseven99@gmail.com>
2023-11-04 19:23:20 +0300

    initialCommit

--- EcommerceProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EcommerceProject.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
